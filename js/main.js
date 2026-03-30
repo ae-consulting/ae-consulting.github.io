6921acf694a903363e397f73e61f523b9ddc76aa
AE CONSULTING <ahmad.essayed@ae.consulting>
2026-03-31 02:53:39 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -128,4 +128,24 @@
     requestAnimationFrame(step);
   }
 
+  // --- Vanta.js Animated Net Background ---
+  if (window.VANTA) {
+    VANTA.NET({
+      el: '#vanta-bg',
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x143127,
+      backgroundColor: 0x0B1221,
+      points: 9,
+      maxDistance: 25,
+      spacing: 15,
+      showDots: true
+    });
+  }
+
 })();
